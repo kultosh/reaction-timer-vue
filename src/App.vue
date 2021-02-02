2c3cba2b83a5ece307bcc201ca80e2a5d1c892bb
kultosh <kulgole90@gmail.com>
2021-02-02 15:01:44 +0545

    Created a block component using events

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
   <h2>Vue Reaction Timer</h2>
+  <button @click="start" :disabled="isplaying">Play</button>
+  <Block v-if="isplaying" />
 </template>
 
 <script>
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
+
   components: {
-    
+    Block
+  },
+
+  data () {
+    return {
+      isplaying: false,
+      delay: null,
+    }
+  },
+
+  methods: {
+    start () {
+      this.delay = 2000 + Math.random() * 5000,
+      this.isplaying = true
+    }
   }
 }
 </script>
