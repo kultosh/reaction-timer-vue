7bcbda159d1d5570a1592d2246a3c11429925f36
kultosh <kulgole90@gmail.com>
2021-02-03 09:41:57 +0545

    Added rank or remark  as per the score in the Result component

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,11 +1,41 @@
 <template>
     <div>
-        <p>Reaction time - {{score}} ms </p>
+        <p v-if="score>0">Reaction time - {{score}} ms </p>
+        <p class="rank">{{rank}}</p>
     </div>
 </template>
 
 <script>
 export default {
     props: ['score'],
+
+    data () {
+        return {
+            rank: null
+        }
+    },
+
+    mounted() {
+        if(this.score <= 200)
+        {
+            this.rank = "You are quick!"
+        }
+        else if (this.score <=400)
+        {
+            this.rank = "You are quite fast!"
+        }
+        else
+        {
+            this.rank = "You are slow as Snail!"
+        }
+    },
 }
 </script>
+
+<style scoped>
+ .rank {
+     font-size: 1.4em;
+     color: #0faf87;
+     font-weight: bold;
+ }
+</style>
